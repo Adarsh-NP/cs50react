dedc6fa288d50379696928110ae7a68276da79c8
Adarsh <f20180806@goa.bits-pilani.ac.in>
2020-05-21 00:35:43 +0530

    about to start react

--- react.js
+++ react.js
@@ -2,3 +2,18 @@
 // declarative implementation detail on how to get it
 // in jsx a tag with a capital start gets invoked as a function
 // single curlies are read as execute as a javascript
+// continues from the node summit video
+import React from 'react'
+
+class MyComponent extends React.Component {
+    onClick(){
+        console.log(this instanceof MyComponent) //prints false
+    }
+    render(){
+    return (
+        <button onClick={this.onClick}>Click Me! </button>
+     )
+    }
+}
+
+
